a1f8da08fa7217391e0b6f93a5cc64c6ad9f0fe4
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-03-15 13:17:09 +0600

    Now, show in the button visited or not visited with style

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,66 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
-
-// {
-//     "name": {
-//         "common": "Curaçao",
-//         "official": "Country of Curaçao"
-//     },
-//     "ccn3": {
-//         "ccn3": "531"
-//     },
-//     "currencies": {
-//         "currencies": {
-//             "ANG": {
-//                 "name": "Netherlands Antillean guilder",
-//                 "symbol": "ƒ"
-//             }
-//         }
-//     },
-//     "capital": {
-//         "capital": [
-//             "Willemstad"
-//         ]
-//     },
-//     "region": {
-//         "region": "Americas"
-//     },
-//     "languages": {
-//         "languages": {
-//             "eng": "English",
-//             "nld": "Dutch",
-//             "pap": "Papiamento"
-//         }
-//     },
-//     "area": {
-//         "area": 444
-//     },
-//     "cca3": {
-//         "cca3": "CUW"
-//     },
-//     "population": {
-//         "population": 155014
-//     },
-//     "continents": {
-//         "continents": [
-//             "North America"
-//         ]
-//     },
-//     "flags": {
-//         "flags": {
-//             "png": "https://flagcdn.com/w320/cw.png",
-//             "svg": "https://flagcdn.com/cw.svg",
-//             "alt": ""
-//         }
-//     }
-// }
-
-const handleVisited = () => {
-    console.log('Country Visited');
-};
 
 const Country = ({country}) => {
     // console.log(country.flags?.flags?.png);
+    const [visited, setVisited] = useState(false);
+
+    
+    const handleVisited = () => {
+        setVisited(!visited);
+        console.log(visited);
+    };
+
 
     return (
         <div className='country'>
@@ -69,7 +19,7 @@ const Country = ({country}) => {
                 <h3>Name: {country.name?.common}</h3>
                 <p>Population: {country.population?.population}</p>
                 <p>{country.area?.area} {country.area?.area > 300000 ? 'Big Country' : 'Small Country'}</p>
-                <button className='cbtn' onClick={handleVisited}>Not Visited</button>
+                <button className={visited ? 'visited' : 'not-visited'} onClick={handleVisited}>{visited ? 'Visited' : 'Not Visited'}</button>
             </div>
         </div>
     );
